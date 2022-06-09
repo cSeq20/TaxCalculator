7da54b008714abe0ece2e4cfbd81dab568034936
Chris Sequeira <chris.seq17@gmail.com>
2022-06-09 21:22:55 +1200

    - code clean up - split functionality out to calculate number of tax brackets in one method and tax in another - added more test cases for the above

--- TaxCalculator/TaxCalculator.cs
+++ TaxCalculator/TaxCalculator.cs
@@ -11,36 +11,34 @@ namespace TaxCalculator
         public Calculator()
         {
             _taxBracket = new List<TaxBracket>();
-            _taxBracket.Add(new TaxBracket { TaxRate = 0.33M, LowerBracket = 0, UpperBracket = 23999 });
-            _taxBracket.Add(new TaxBracket { TaxRate = 0.2M, LowerBracket = 24000, UpperBracket = 47999 });
-            _taxBracket.Add(new TaxBracket { TaxRate = 0.175M, LowerBracket = 48000, UpperBracket = 79999 });
+            _taxBracket.Add(new TaxBracket { TaxRate = 0.33M, LowerBracket = 0, UpperBracket = 24000 });
+            _taxBracket.Add(new TaxBracket { TaxRate = 0.2M, LowerBracket = 24000, UpperBracket = 48000 });
+            _taxBracket.Add(new TaxBracket { TaxRate = 0.175M, LowerBracket = 48000, UpperBracket = 80000 });
             _taxBracket.Add(new TaxBracket { TaxRate = 0.48M, LowerBracket = 80000 });
         }
 
-        public decimal Calculate(decimal input, bool taxExempt = false)
+        public decimal Calculate(decimal earnings, int numberOfBrackets)
         {
+            var earningsRounded = Math.Floor(earnings);
             var taxAmount = 0.00M;
-            var remainToCalculate = input;
-            var count = 0;
-            _taxBracket[_taxBracket.Count - 1].UpperBracket = input;
+            _taxBracket[_taxBracket.Count - 1].UpperBracket = earnings;
 
-            do
+            for (int i = 0; i <= numberOfBrackets; i++)
             {
-                // input = 150,000
-                // should go through 4 tax brackets
-                // first loop - taxAmount = 23999 * 0.33
-                // 
-                // break
-                var test = _taxBracket[count].UpperBracket < input 
-                    ? _taxBracket[count].UpperBracket - _taxBracket[count].LowerBracket
-                    : input - _taxBracket[count].LowerBracket;
-
-                taxAmount += test * _taxBracket[count].TaxRate;
-                count++;
+                var bandAmount = earningsRounded > _taxBracket[i].UpperBracket ? _taxBracket[i].UpperBracket - _taxBracket[i].LowerBracket : earningsRounded - _taxBracket[i].LowerBracket;
+                taxAmount += bandAmount * _taxBracket[i].TaxRate;
             }
-            while (input > _taxBracket[count].LowerBracket && count <= _taxBracket.Count);
 
             return taxAmount;
+        }
+
+        public int NumberOfBracketsBasedOnEarnings(decimal earnings, bool taxExempt = false)
+        {
+            var earningsRounded = Math.Floor(earnings);
+
+            return earningsRounded <= 0 || taxExempt ? -1
+                : earningsRounded > _taxBracket.Max(x => x.LowerBracket) ? _taxBracket.Count - 1
+                : _taxBracket.FindIndex(x => x.LowerBracket <= earningsRounded && x.UpperBracket >= earningsRounded);
         }
     }
 }
