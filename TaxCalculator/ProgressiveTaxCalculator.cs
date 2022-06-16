b279087414570031aff5daaf4dd091a8657d34bf
Chris Sequeira <chris.seq17@gmail.com>
2022-06-16 19:18:52 +1200

    refactoring and removing unneeded test cases

--- TaxCalculator/ProgressiveTaxCalculator.cs
+++ TaxCalculator/ProgressiveTaxCalculator.cs
@@ -6,40 +6,36 @@ namespace TaxCalculator
 {
     public class ProgressiveTaxCalculator
     {
-        private List<TaxBracket> _taxBracket { get; set; }
+        private readonly List<TaxBracket> taxBracket;
 
         public ProgressiveTaxCalculator()
         {
-            _taxBracket = new List<TaxBracket>();
-            _taxBracket.Add(new TaxBracket { TaxRate = 0.33M, LowerBracket = 0, UpperBracket = 24000 });
-            _taxBracket.Add(new TaxBracket { TaxRate = 0.175M, LowerBracket = 48000, UpperBracket = 80000 });
-            _taxBracket.Add(new TaxBracket { TaxRate = 0.2M, LowerBracket = 24000, UpperBracket = 48000 });
-            _taxBracket.Add(new TaxBracket { TaxRate = 0.48M, LowerBracket = 80000 });
-            _taxBracket = _taxBracket.OrderBy(bracket => bracket.LowerBracket).ToList();
+            taxBracket = new List<TaxBracket>();
+            taxBracket.Add(new TaxBracket { TaxRate = 0.33M, LowerBracket = 0, UpperBracket = 24000 });
+            taxBracket.Add(new TaxBracket { TaxRate = 0.175M, LowerBracket = 48000, UpperBracket = 80000 });
+            taxBracket.Add(new TaxBracket { TaxRate = 0.2M, LowerBracket = 24000, UpperBracket = 48000 });
+            taxBracket.Add(new TaxBracket { TaxRate = 0.48M, LowerBracket = 80000, UpperBracket = decimal.MaxValue });
+            taxBracket = taxBracket.OrderBy(bracket => bracket.LowerBracket).ToList();
         }
 
-        public decimal Calculate(decimal earnings, int numberOfBrackets)
+        public decimal Calculate(decimal earnings, bool taxExempt = false)
         {
             var earningsRounded = Math.Floor(earnings);
             var taxAmount = 0.00M;
-            _taxBracket[_taxBracket.Count - 1].UpperBracket = earnings;
+            if (taxExempt) return taxAmount;
 
-            for (int i = 0; i <= numberOfBrackets; i++)
+            foreach (var bracket in taxBracket.Where(brk => brk.LowerBracket < earnings))
             {
-                var bandAmount = earningsRounded > _taxBracket[i].UpperBracket ? _taxBracket[i].UpperBracket - _taxBracket[i].LowerBracket : earningsRounded - _taxBracket[i].LowerBracket;
-                taxAmount += bandAmount * _taxBracket[i].TaxRate;
+                taxAmount += CalculateTaxForBracket(bracket, earningsRounded);
             }
 
             return taxAmount;
         }
 
-        public int NumberOfBracketsBasedOnEarnings(decimal earnings, bool taxExempt = false)
+        private decimal CalculateTaxForBracket(TaxBracket bracket, decimal earnings)
         {
-            var earningsRounded = Math.Floor(earnings);
-
-            return earningsRounded <= 0 || taxExempt ? -1
-                : earningsRounded > _taxBracket.Max(x => x.LowerBracket) ? _taxBracket.Count - 1
-                : _taxBracket.FindIndex(x => x.LowerBracket <= earningsRounded && x.UpperBracket >= earningsRounded);
+            var bandAmount = earnings > bracket.UpperBracket ? bracket.UpperBracket - bracket.LowerBracket : earnings - bracket.LowerBracket;
+            return bandAmount * bracket.TaxRate;
         }
     }
 }
--- TaxCalculatorTests/TaxCalculatorTests.cs
+++ TaxCalculatorTests/TaxCalculatorTests.cs
@@ -7,116 +7,60 @@ namespace TaxCalculatorTests
     public class TaxCalculatorTests
     {
         [Fact]
-        public void Test_Number_Of_Brackets_Is_Negative_Based_On_Zero_Earnings()
-        {
-            var taxCalculator = new ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(0);
-            Assert.Equal(-1, numberOfBrackets);
-        }
-
-        [Fact]
-        public void Test_Number_Of_Brackets_Is_Negative_When_Tax_Exempt()
-        {
-            var taxCalculator = new ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(50000, true);
-            Assert.Equal(-1, numberOfBrackets);
-        }
-
-        [Fact]
-        public void Test_Earnings_Is_Within_First_Bracket()
-        {
-            var taxCalculator = new ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(20000);
-            Assert.Equal(0, numberOfBrackets);
-        }
-
-        [Fact]
-        public void Test_Earnings_Is_Within_Second_Bracket()
-        {
-            var taxCalculator = new ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(40000);
-            Assert.Equal(1, numberOfBrackets);
-        }
-
-        [Fact]
-        public void Test_Earnings_Is_Within_Third_Bracket()
-        {
-            var taxCalculator = new ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(60000);
-            Assert.Equal(2, numberOfBrackets);
-        }
-
-        [Fact]
-        public void Test_Earnings_Is_Within_Fourth_Bracket()
-        {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(150000);
-            Assert.Equal(3, numberOfBrackets);
-        }
-
-        [Fact]
         public void Test_tax_is_zero_given_earnings_are_zero()
         {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(0);
-            var taxAmount = taxCalculator.Calculate(0, numberOfBrackets);
+            var taxCalculator = new ProgressiveTaxCalculator();
+            var taxAmount = taxCalculator.Calculate(0);
             Assert.Equal(0, taxAmount);
         }
 
         [Fact]
         public void Test_tax_is_zero_if_earner_is_tax_exempt()
         {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(50000, true);
-            var taxAmount = taxCalculator.Calculate(50000, numberOfBrackets);
+            var taxCalculator = new ProgressiveTaxCalculator();
+            var taxAmount = taxCalculator.Calculate(50000, true);
             Assert.Equal(0, taxAmount);
         }
 
         [Fact]
         public void Test_tax_calculation_if_earnings_input_is_decimal()
         {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBracketsWithDecimal = taxCalculator.NumberOfBracketsBasedOnEarnings(0);
-            var taxAmountWithDecimal = taxCalculator.Calculate(50000.65M, numberOfBracketsWithDecimal);
-
-            var numberOfBracketsWithoutDecimal = taxCalculator.NumberOfBracketsBasedOnEarnings(0);
-            var taxAmountWithoutDecimal = taxCalculator.Calculate(50000, numberOfBracketsWithoutDecimal); 
+            var taxCalculator = new ProgressiveTaxCalculator();
+            var taxAmountWithDecimal = taxCalculator.Calculate(50000.65M);
+            
+            var taxAmountWithoutDecimal = taxCalculator.Calculate(50000); 
             Assert.Equal(taxAmountWithDecimal, taxAmountWithoutDecimal);
         }
 
         [Fact]
         public void Test_tax_calculation_if_earnings_in_lowest_bracket()
         {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(20000);
-            var taxAmount = taxCalculator.Calculate(20000, numberOfBrackets);
+            var taxCalculator = new ProgressiveTaxCalculator();
+            var taxAmount = taxCalculator.Calculate(20000);
             Assert.Equal(6600, taxAmount);
         }
 
         [Fact]
         public void Test_tax_calculation_if_earnings_in_second_bracket()
         {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(40000);
-            var taxAmount = taxCalculator.Calculate(40000, numberOfBrackets);
+            var taxCalculator = new ProgressiveTaxCalculator();
+            var taxAmount = taxCalculator.Calculate(40000);
             Assert.Equal(11120, taxAmount);
         }
 
         [Fact]
         public void Test_tax_calculation_if_earnings_in_third_bracket()
         {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(60000);
-            var taxAmount = taxCalculator.Calculate(60000, numberOfBrackets);
+            var taxCalculator = new ProgressiveTaxCalculator();
+            var taxAmount = taxCalculator.Calculate(60000);
             Assert.Equal(14820, taxAmount);
         }
 
         [Fact]
         public void Test_tax_calculation_if_earnings_in_fourth_bracket()
         {
-            var taxCalculator = new TaxCalculator.ProgressiveTaxCalculator();
-            var numberOfBrackets = taxCalculator.NumberOfBracketsBasedOnEarnings(150000);
-            var taxAmount = taxCalculator.Calculate(150000, numberOfBrackets); //chris' salary
+            var taxCalculator = new ProgressiveTaxCalculator();
+            var taxAmount = taxCalculator.Calculate(150000); //chris' salary
             Assert.Equal(51920, taxAmount);
         }
     }
